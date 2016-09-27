6bdba7f548e226ae80b3e62f7b2cf8f517985122
David Reagan <david@reagannetworks.com>
2016-09-27 12:04:14 -0700

    Ah, might help to run the correct script...

--- scripts/deploymaster.sh
+++ scripts/deploymaster.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-/usr/bin/sudo /bin/bash $TRAVIS_BUILD_DIR/scripts/drupal-fix-perms.sh --drupal_path=$TRAVIS_BUILD_DIR/public_html --drupal_user=$dojouserid --httpd_group=$nobodyuid;
+/usr/bin/sudo /bin/bash $TRAVIS_BUILD_DIR/scripts/drupal-set-permissions.sh --drupal_path=$TRAVIS_BUILD_DIR/public_html --drupal_user=$dojouserid --httpd_group=$nobodyuid;
 
 #rsync -apqz -e "ssh -p 22123 -o 'StrictHostKeyChecking no' -o 'PasswordAuthentication no' -o 'PubkeyAuthentication yes'" $TRAVIS_BUILD_DIR/public_html/ dojomeister@coderdojo.lanecc.edu:~/www;
 
