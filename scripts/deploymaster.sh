209c22957aceedb09c46d71039532cc6a7121e63
David Reagan <david@reagannetworks.com>
2016-09-29 14:59:30 -0700

    In scripts/deploymaster.sh remove -v from rsync command and add -q to it.

--- scripts/deploymaster.sh
+++ scripts/deploymaster.sh
@@ -4,6 +4,6 @@ ssh -p 22123 -t -t -o 'StrictHostKeyChecking no' -o 'PasswordAuthentication no' 
   /bin/git fetch origin
   /bin/git reset --hard origin/master
   /bin/bash /home/$dojousername/staging/eugenecoderdojo/scripts/drupal-set-permissions.sh --drupal_path=/home/$dojousername/staging/eugenecoderdojo/public_html --drupal_user=$dojousername
-  /bin/rsync --exclude=".git" -avz /home/$dojousername/staging/eugenecoderdojo/public_html/ /home/$dojousername/public_html
+  /bin/rsync --exclude=".git" -aqz /home/$dojousername/staging/eugenecoderdojo/public_html/ /home/$dojousername/public_html
   exit
 EOF
